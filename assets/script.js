4179d35fe4e1739a823228e76d56a264b10faf9d
akash301994 <patel.akash30@icloud.com>
2023-05-09 14:19:49 -0400

    added if statements

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,42 @@
 // Assignment code here
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowerCase = "abcdefghijklmopqrstuvwxyz";
+var upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCaseChars = "abcdefghijklmopqrstuvwxyz";
 var specialChars = "!@#$%^&*()_+-=[];',./?><|:`~}{";
 var numbers = "1234567890";
 
 
-// making the function called generatePassword.
-//just making a console log so i can keep track. /
-function generatePassword() {
-  console.log("hey you click the button");
+//need function that will randomize characters
+function randomizer(text, numbers = 1) {
+  let randomString = "";
 
-  //pseudo code
+  for (let i = 0; i < numbers; i++) {
+    randomString += text.charAt(Math.floor(Math.random() * text.length));
+  }
+
+  return randomString;
+};
+
+
+// making the function called generatePassword.
+//just making a console log so i can keep track. 
+function generatePassword() {
+  console.log("hey you clicked the button");
+  let includeChars = "";
+  var passwordGenerated = "";
+
+
  // 1. prompt the user for the password criteria
  //-pass length 8-128
    //-lowercase, uppercase, numbers, special character
 
- includeNumberChars = confirm ('click ok to incude numbers in your password');
- includeUpperCaseChars = confirm ('click ok to incude upper case letters in your password');
- includeLowerCaseChars = confirm ('click ok to incude lower case numbers in your password');
- includeSpecialChars = confirm ('click ok to incude special characters in your password');
-
-//need function that will randomize characters
+ includeNumbersChars = confirm ('Click "OK" to include numbers in your password OR click cancel to exclude');
+ includeUpperCaseChars = confirm ('Click "OK" to include upper case letters in your password OR click cancel to exclude');
+ includeLowerCaseChars = confirm ('Click "OK" to include lower case numbers in your password OR click cancel to exclude');
+ includeSpecialChars = confirm ('Click "OK" to include special characters in your password OR click cancel to exclude');
 
 
-
-
-
-let passwordLength = parseInt(
+ //prompting user for length of pass to meet user criteria
+ let passwordLength = parseInt(
   prompt("How many characters would you like in your passord? Must be greater than 8 and less than 128!")
 );
   
@@ -37,13 +47,39 @@ if (isNaN(passwordLength)|| passwordLength <8 || passwordLength >128) {
 
 
 
-
-
-// 3. Generate password based on criteria
-// 4. display passoword. 
-
-  return password;
+ // If characters are selected then they are run through the randomizer function that will randomize character outcomes
+ if (includeNumbersChars ) {
+  includeChars += numbers;
+  passwordGenerated += randomizer(numbers);
 }
+
+if (includeUpperCaseChars ) {
+  includeChars += upperCaseChars;
+  passwordGenerated += randomizer(upperCaseChars);
+} 
+
+ if (includeLowerCaseChars) {
+  includeChars += lowerCaseChars;
+  passwordGenerated += randomizer(lowerCaseChars);
+}
+
+if (includeSpecialChars) {
+  includeChars += specialChars;
+  passwordGenerated += randomizer(specialChars);
+}
+
+passwordGenerated += randomizer(
+  includeChars, passwordLength - passwordGenerated.length
+);
+
+passwordGenerated = randomizer(
+  passwordGenerated, passwordGenerated.length);
+
+
+// 4. display password. 
+
+  return passwordGenerated;
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
